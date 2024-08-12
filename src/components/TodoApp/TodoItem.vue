22f1041814b61a10e646764b9edd14af4044559b
xkyong <xky13802315708@163.com>
2024-08-12 21:37:01 +0800

    feat: 完善测试用例和功能逻辑

--- src/components/TodoApp/TodoItem.vue
+++ src/components/TodoApp/TodoItem.vue
@@ -1,14 +1,15 @@
 <template>
-  <li data-testid="todo-item" :class="{ 'completed': todo.done }">
+  <li data-testid="todo-item" :class="{ 'completed': todo.done, 'editing': isEditing }">
     <div class="view">
       <input v-model="todo.done" data-testid="todo-done" class="toggle" type="checkbox" />
-      <label data-testid="todo-text">{{ todo.text }}</label>
+      <label data-testid="todo-text" @dblclick="isEditing = true">{{ todo.text }}</label>
       <button data-testid="delete" class="destroy" @click="$emit('delete-todo', todo.id)"></button>
     </div>
-    <input class="edit" value="Rule the web" />
+    <input v-focus="todo.text" data-testid="todo-edit" class="edit" :value="todo.text" @blur="isEditing = false" @keyup.enter="handleEditTodo" />
   </li>
 </template>
 <script setup lang="ts">
+import { ref, type DirectiveBinding } from 'vue'
 interface TodoItem {
   id: number
   text: string
@@ -25,5 +26,21 @@ const props = withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits<{
   (e: 'delete-todo', id: number): void
+  (e: 'edit-todo', data: { id: number; text: string }): void
 }>()
+
+const isEditing = ref(false)
+
+const vFocus = (el: HTMLInputElement, binding: DirectiveBinding<string>) => {
+  if (binding.value) {
+    el.focus()
+  }
+}
+
+function handleEditTodo(e: KeyboardEvent) {
+  emit('edit-todo', {
+    id: props.todo.id,
+    text: (e.target as HTMLInputElement).value
+  })
+}
 </script>
--- src/components/TodoApp/tests/TodoItem.test.ts
+++ src/components/TodoApp/tests/TodoItem.test.ts
@@ -36,4 +36,38 @@ describe('TodoItem.vue', () => {
     expect(wrapper!.emitted()['delete-todo']).toBeTruthy()
     expect((wrapper!.emitted()['delete-todo'] as any[])[0][0]).toBe(todo.id)
   })
+
+  it('edit todo', async () => {
+    const label = wrapper!.find('[data-testid="todo-text"]')
+    const todoItem = wrapper!.find('[data-testid="todo-item"]')
+    const todoEdit = wrapper!.find('[data-testid="todo-edit"]')
+    await label.trigger('dblclick')
+    expect(todoItem.classes()).toContain('editing')
+
+    await todoEdit.trigger('blur')
+    expect(todoItem.classes()).not.toContain('editing')
+  })
+
+  it('save edit todo', async () => {
+    const label = wrapper!.find('[data-testid="todo-text"]')
+    const todoEdit = wrapper!.find('[data-testid="todo-edit"]')
+    await label.trigger('dblclick')
+    
+    // 编辑文本框中的内容展示
+    expect((todoEdit.element as HTMLInputElement).value).toBe((wrapper!.vm as InstanceType<typeof TodoItem>).todo.text)
+
+    const text = 'hello'
+    await todoEdit.setValue(text)
+
+    // 触发回车保存事件
+    await todoEdit.trigger('keyup.enter')
+
+    // 数据被修改了，向父组件发布事件
+    // expect((wrapper!.vm as InstanceType<typeof TodoItem>).todo.text).toBe(text)
+    expect(wrapper!.emitted()['edit-todo']).toBeTruthy()
+    expect((wrapper!.emitted()['edit-todo'] as  any[])[0][0]).toEqual({
+      id: (wrapper!.vm as InstanceType<typeof TodoItem>).todo.id,
+      text
+    })
+  })
 })
