0b9dafdd1de27789053d46befa3672058914307a
xkyong <xky13802315708@163.com>
2024-08-17 18:53:48 +0800

    chore: 补充 tsconfig.json 文件 paths 字段说明

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
+// tsconfig.json 中需要配置 paths 关于 `@/*` 的别名，
+// 否则会出现 `Cannot find module '@/components/TodoApp/index.vue' or its corresponding type declarations.`
 import TodoApp from '@/components/TodoApp/index.vue'
 
 const routes: readonly RouteRecordRaw[] = [
