21e7e2e4d8bb8b81c83a83e6a363b1df039b6919
xkyong <xky13802315708@163.com>
2024-08-17 21:44:12 +0800

    test: 配置测试覆盖率脚本

--- src/components/TodoApp/tests/TodoHeader.test.ts
+++ src/components/TodoApp/tests/TodoHeader.test.ts
@@ -14,6 +14,15 @@ describe('TodoHeader.vue', () => {
     expect((newTodoInput.element as HTMLInputElement).value).toBe('')
   })
 
+  it('New todo with empty text', async () => {
+    const wrapper = shallowMount(TodoHeader)
+    const newTodoInput = wrapper.find('input[data-testid="new-todo"]')
+    const text = ''
+    await newTodoInput.setValue(text)
+    await newTodoInput.trigger('keyup.enter')
+    expect(wrapper.emitted()['new-todo']).toBeFalsy()
+  })
+
   it('snapshot', () => {
     const wrapper = shallowMount(TodoHeader)
     expect(wrapper.html()).toMatchSnapshot()
